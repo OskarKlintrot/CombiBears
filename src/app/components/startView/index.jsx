0a975583adf34628b1f254330e991b7eadfccf8e
OskarKlintrot <oskar.klintrot@gmail.com>
2016-02-08 11:23:46 +0100

    Added a container for the startview

---
+++ src/app/components/startView/index.jsx
@@ -0,0 +1,23 @@
+import React, {PropTypes} from 'react';
+import {Link} from 'react-router';
+
+class StartView extends React.Component {
+  render() {
+    return (
+      <div>
+        <img id = 'Logotyp' alt = 'Logotyp' src = ''></img>
+        <options>
+          <SofaOptions />
+        </options>
+        <options>
+          <BearOptions />
+        </options>
+        <img id = 'StartButton' alt = 'StartButton' src = ''></img>
+        <input type = 'button'></input>
+        <infoFlash />
+      </div>
+    );
+  }
+}
+
+export default StartView;
