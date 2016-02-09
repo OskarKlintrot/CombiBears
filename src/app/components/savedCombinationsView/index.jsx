d452a3ad7c71db515b68fd8dd81ac09b62eb5c07
Lisa Westlund <lw222gu@student.lnu.se>
2016-02-09 09:01:36 +0100

    Added savedCombinationsView component

---
+++ src/app/components/savedCombinationsView/index.jsx
@@ -0,0 +1,14 @@
+import React, { PropTypes } from 'react'
+import { Link } from 'react-router'
+
+class SavedCombinationsView extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Hej!</p>
+      </div>
+    )
+  }
+}
+
+export default SavedCombinationsView
--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -5,11 +5,13 @@ import Wrapper from './components/shared/wrapper.jsx';
 import NoMatch from './components/shared/noMatch.jsx';
 import Main from './components/main.jsx';
 import About from './components/about.jsx';
+import SavedCombinationsView from './components/savedCombinationsView';
 
 export default (
   <Route path = '/' component = {Wrapper}>
     <IndexRoute component = {Main} />
     <Route path = '/about' component = {About}/>
+    <Route path = '/savedCombinations' component = {SavedCombinationsView} />
     <Route path = '*' component = {NoMatch}/>
   </Route>
 );
