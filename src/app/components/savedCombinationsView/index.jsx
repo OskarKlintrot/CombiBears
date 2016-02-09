08486b121879d0750f4d4e507ba42e25d7b01e22
OskarKlintrot <oskar.klintrot@gmail.com>
2016-02-09 09:47:10 +0100

    Merge with dev

--- src/app/components/savedCombinationsView/index.jsx
+++ src/app/components/savedCombinationsView/index.jsx
@@ -1,13 +1,13 @@
-import React, { PropTypes } from 'react'
-import { Link } from 'react-router'
+import React from 'react'
+// import { Link } from 'react-router'
 
 class SavedCombinationsView extends React.Component {
   render() {
     return (
       <div>
-        <img src="public/pics/icons/new-sofa.png" alt="Icon for new sofa" />
-        <img src="public/pics/icons/show-result.png" alt="Icon for showing result" />
-        <img src="public/pics/icons/arrow-right.png" alt="Icon for returning to game view" />
+        <img src = 'public/pics/icons/new-sofa.png' alt = 'Icon for new sofa' />
+        <img src = 'public/pics/icons/show-result.png' alt = 'Icon for showing result' />
+        <img src = 'public/pics/icons/arrow-right.png' alt = 'Icon for returning to game view' />
       </div>
     )
   }
