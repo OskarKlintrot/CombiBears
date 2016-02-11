83969b6480f05f1fcb923200171514403a427fe4
Daniel Vedin <dv222bk@student.lnu.se>
2016-02-11 16:41:56 +0100

    bearOptions, colorPicker
    First versions of bearOptions and colorPicker.

--- src/app/components/startView/bearOptions.jsx
+++ src/app/components/startView/bearOptions.jsx
@@ -1,7 +1,39 @@
 import React from 'react'
+// import Bears from '../../shared/bears/'
+import ColorPicker from './colorPicker'
 
-const BearOptions = () => {
-  return <p>BearOptions</p>
+class BearOptions extends React.Component {
+  render() {
+    return (
+      <div id = 'bearOptions'>
+        <img id = 'bear1' onClick = {
+            ColorPicker.handleClick( this )
+          }
+        />
+        <img id = 'bear2' onClick = {
+            ColorPicker.handleClick( this )
+          }
+        />
+        <img id = 'bear3' onClick = {
+            ColorPicker.handleClick( this )
+          }
+        />
+        <img id = 'bear4' onClick = {
+            ColorPicker.handleClick( this )
+          }
+        />
+      </div>
+      /* <Bears>
+        { this.props.map( ( bear, key ) => {
+          return (
+            <div onClick = {
+                ColorPicker.handleClick( bear ) } key = { key }
+            >{ bear }</div>
+          )
+        }) }
+      </Bears> */
+    )
+  }
 }
 
 export default BearOptions
