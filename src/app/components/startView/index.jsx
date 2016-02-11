e2f2a2ad14830ae07b2ceff6af2921a57f7501ca
OskarKlintrot <oskar.klintrot@gmail.com>
2016-02-11 11:17:35 +0100

    Info flash

--- src/app/components/startView/index.jsx
+++ src/app/components/startView/index.jsx
@@ -1,10 +1,40 @@
 import React /* , { PropTypes }*/ from 'react'
-// import { Link } from 'react-router'
+import { Link } from 'react-router'
 import SofaOptions from './sofaOptions'
 import BearOptions from './bearOptions'
 import InfoFlash from './infoFlash'
 
 class StartView extends React.Component {
+  constructor( props ) {
+    super( props )
+    /* One way of using this.setState() when using ES2015 classes
+     * is to either bind this here in the constructor...
+     */
+    this.onCloseModal = this.onCloseModal.bind( this )
+    this.onModalCloseRequest = this.onModalCloseRequest.bind( this )
+    this.state = {
+      modalIsOpen: false
+    }
+  }
+
+  /* ...or binding this here using an arrow function. I don't know if there
+   * is a best practice or ESLint rules for this "issue". Note the
+   * semicolon that must be on the end of a class property!
+   */
+  onOpenModal = () => {
+    this.setState({ modalIsOpen: true })
+  };
+
+  onCloseModal() {
+    this.setState({ modalIsOpen: false })
+  }
+
+  onModalCloseRequest() {
+    // Opportunity to validate something and keep the
+    // modal open even if it requested to be closed
+    this.setState({ modalIsOpen: false })
+  }
+
   render() {
     return (
       <div>
@@ -15,9 +45,21 @@ class StartView extends React.Component {
         <div className = 'option'>
           <BearOptions />
         </div>
-        <img id = 'StartButton' alt = 'StartButton' src = ''></img>
-        <input type = 'button'></input>
-        <InfoFlash />
+        <Link to = { '/start' }>
+          <img
+            id = 'StartButton'
+            alt = 'StartButton'
+            src = '/public/pics/icons/start.png'
+            height = '50px'
+            width = '150px'
+          ></img>
+        </Link>
+        <InfoFlash
+          handleOpenModal = { this.onOpenModal }
+          handleCloseModal = { this.onCloseModal }
+          handleModalCloseRequest = { this.onModalCloseRequest }
+          open = { this.state.modalIsOpen }
+        />
       </div>
     )
   }
--- src/app/components/startView/infoFlash.jsx
+++ src/app/components/startView/infoFlash.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
+import Modal from 'react-modal'
 
-const InfoFlash = () => {
-  return <p>InfoFlash</p>
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+}
+
+
+const InfoFlash = ( props ) => {
+  const {
+    open,
+    handleOpenModal,
+    handleCloseModal,
+    handleModalCloseRequest
+  } = props
+
+  return (
+    <div>
+      <input
+        type = 'button'
+        value = 'Info'
+        onClick = { handleOpenModal }
+      />
+      <Modal
+        isOpen = { open }
+        onRequestClose = { handleModalCloseRequest }
+        style = { customStyles }
+      >
+        <h1>Info flash</h1>
+        <p>Some information for the teachers and parents.</p>
+        <input
+          type = 'button'
+          value = 'X'
+          onClick = { handleCloseModal }
+        />
+      </Modal>
+    </div>
+  )
+}
+
+InfoFlash.propTypes = {
+  open: PropTypes.bool.isRequired,
+  handleOpenModal: PropTypes.func.isRequired,
+  handleCloseModal: PropTypes.func.isRequired,
+  handleModalCloseRequest: PropTypes.func.isRequired
 }
 
 export default InfoFlash
