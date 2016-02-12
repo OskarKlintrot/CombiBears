1beae44718ab5275c6f33cec1feeab85e2d06f73
OskarKlintrot <oskar.klintrot@gmail.com>
2016-02-12 14:06:15 +0100

    Syntax

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -4,16 +4,16 @@ import Wrapper from './components/shared/wrapper.jsx'
 
 import NoMatch from './components/shared/noMatch.jsx'
 import Main from './components/main.jsx'
-import StartView from './components/startView'
 import About from './components/about.jsx'
+import StartView from './components/startView'
 import SavedCombinationsView from './components/savedCombinationsView'
 
 export default (
   <Route path='/' component={ Wrapper }>
     <IndexRoute component={ Main } />
     <Route path='/start' component={ StartView }/>
-    <Route path='/about' component={ About }/>
     <Route path='/saved' component={ SavedCombinationsView } />
+    <Route path='/about' component={ About }/>
     <Route path='*' component={ NoMatch }/>
   </Route>
 )
