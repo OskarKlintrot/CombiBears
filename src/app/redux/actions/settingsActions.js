1a9d08f3d8bc101486be13703f636eb3ad57432f
OskarKlintrot <oskar.klintrot@gmail.com>
2016-02-12 10:35:11 +0100

    Added Action and Reducer for selection number of seats in the couch

--- src/app/redux/actions/settingsActions.js
+++ src/app/redux/actions/settingsActions.js
@@ -1,7 +1,8 @@
 import ActionTypes from "./actionTypes"
 
 const {
-	RESET
+	RESET_SETTINGS,
+  SET_NUMBER_OF_SEATS
 	// Write down the actions you want to use here
 } = ActionTypes
 
@@ -9,7 +10,15 @@ const SettingsActions = {
   resetSettings: () => {
     return ( dispatch ) => {
       dispatch({
-        type: RESET
+        type: RESET_SETTINGS
+      })
+    }
+  },
+  setNumberOfSeats: ( numberOfSeats ) => {
+    return ( dispatch ) => {
+      dispatch({
+        type: SET_NUMBER_OF_SEATS,
+        numberOfSeats: numberOfSeats
       })
     }
   }
