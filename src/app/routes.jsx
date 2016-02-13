6d49ada2db48793ba22d3762bc94c18df8cacdd7
Johnny Pesola <johnny@pesola.eu>
2016-02-13 18:40:16 +0100

    More modifications to fit master

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -5,13 +5,17 @@ import Wrapper from './components/shared/wrapper.jsx'
 import NoMatch from './components/shared/noMatch.jsx'
 import Main from './components/main.jsx'
 import About from './components/about.jsx'
+import StartView from './components/startView'
+import SavedCombinationsView from './components/savedCombinationsView'
 import GameViewPixi from './components/gameViewPixi'
 
 export default (
-  <Route path = '/' component = { Wrapper }>
-    <IndexRoute component = { Main } />
+  <Route path='/' component={ Wrapper }>
+    <IndexRoute component={ Main } />
+    <Route path='/start' component={ StartView }/>
+    <Route path='/saved' component={ SavedCombinationsView } />
     <Route path='/game-pixi' component = { GameViewPixi } />
-    <Route path='/about' component = { About }/>
-    <Route path='*' component = { NoMatch }/>
+    <Route path='/about' component={ About }/>
+    <Route path='*' component={ NoMatch }/>
   </Route>
 )
