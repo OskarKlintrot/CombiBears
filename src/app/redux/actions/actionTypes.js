175bf7874dd53bb9616cb816311b65ebce59d3c1
OskarKlintrot <oskar.klintrot@gmail.com>
2016-02-15 11:04:21 +0100

    Refactor: ActionTypes made immutable

--- src/app/redux/actions/actionTypes.js
+++ src/app/redux/actions/actionTypes.js
@@ -1,15 +1,17 @@
-export const ActionTypesApp = {
+const App = {
   RESET: 'RESET',
   SET_USER: 'SET_USER'
   // You can have several objects here...
 }
 
-export const ActionTypesSettings = {
+const Settings = {
   RESET_SETTINGS: 'RESET_SETTINGS',
   SET_NUMBER_OF_SEATS: 'SET_NUMBER_OF_SEATS'
   // ...just to increase code readability
 }
 
-const ActionTypes = Object.assign({}, ActionTypesApp, ActionTypesSettings )
+const ActionTypes = Object.assign({}, App, Settings )
 
-export default ActionTypes
+export const ActionTypesApp = Object.freeze( App )
+export const ActionTypesSettings = Object.freeze( Settings )
+export default Object.freeze( ActionTypes )
