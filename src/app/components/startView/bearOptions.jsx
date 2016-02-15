c1c66ea8f37e72ec3a8fad60225ab5e370bfb93b
Daniel Vedin <dv222bk@student.lnu.se>
2016-02-15 21:52:21 +0100

    Removed bears
    Removed bears and moved the code into bearOptions instead.

--- src/app/components/startView/bearOptions.jsx
+++ src/app/components/startView/bearOptions.jsx
@@ -1,12 +1,38 @@
 import React from 'react'
-import Bears from './bears'
+import Bear from './bear'
 
-class BearOptions extends React.Component {
-  render() {
-    return (
-      <Bears />
-    )
+const style = {
+  box: {
+    position: 'relative'
+  },
+  bear: {
+    position: 'relative'
   }
+}
+
+const colors = [
+  'blue',
+  'green',
+  'placeholder',
+  'placeholder'
+]
+
+const BearOptions = () => {
+  return (
+    <div
+      className='bears'
+      style={ style.box }
+    >
+      { colors.map( ( color, key ) => {
+        return (
+          <Bear
+            color={ color }
+            key={ key }
+          />
+        )
+      }) }
+    </div>
+  )
 }
 
 export default BearOptions
