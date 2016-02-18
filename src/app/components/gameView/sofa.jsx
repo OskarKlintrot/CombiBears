aa2dd7d2e1345ad4e148283cfe8e89319b92c3d9
Johnny Pesola <johnny@pesola.eu>
2016-02-18 02:43:51 +0100

    Sofa now adjusts background image after seat count

--- src/app/components/gameView/sofa.jsx
+++ src/app/components/gameView/sofa.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import Seat from './seat'
 
 class Sofa extends React.Component {
+
+  getSeatsImage() {
+    switch ( this.props.children.length ) {
+    case 2: return 'two'
+    case 3: return 'three'
+    case 4: return 'four'
+    default: return 'four'
+    }
+  }
+
   render() {
 
     const styles = {
       sofa: {
         height: '210px',
         border: '1px solid #00f',
-        background: 'url(public/pics/sofas/three.png)',
-        backgroundSize: '400px',
+        background: 'url(public/pics/sofas/' + this.getSeatsImage() + '.png)',
+        backgroundSize: 'contain',
         backgroundRepeat: 'no-repeat'
       }
     }
