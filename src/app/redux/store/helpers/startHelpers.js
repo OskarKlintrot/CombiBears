2d63080573ee49acbc28d20f81005aa45365e872
Daniel Vedin <dv222bk@student.lnu.se>
2016-02-28 19:54:07 +0100

    Delete and update checks
    Actions now checks for specific conditions before deleting and updating
    a bear slot.
    
    Before deleting: Make sure there is at least 2 bears left after
    deleting, otherwise don't delete.
    
    Before updating: Make sure the color does not already exist.

--- src/app/redux/store/helpers/startHelpers.js
+++ src/app/redux/store/helpers/startHelpers.js
@@ -1,12 +1,26 @@
 export const updateBear = ( bears, bearSrc, bearId ) => {
+  // Fail-safe, checking if the color already exists on another bear
+  for ( let bear = 0; bear < bears.length; bear += 1 ) {
+    if ( bears[bear].src === bearSrc && bear !== bearId )
+      // If it exists, don't change the state
+      return bears
+  }
+
   bears[bearId] = { src: bearSrc }
 
   return bears
 }
 
 export const deleteBear = ( bears, bearId ) => {
-  // TO DO: Check if there are 3 or more bears before deleting
-  bears[bearId] = null
+  let numberOfBears = 0
+
+  for ( let bear = 0; bear < bears.length; bear += 1 ) {
+    if ( bears[bear] !== null )
+      numberOfBears += 1
+  }
+
+  if ( numberOfBears > 2 )
+    bears[bearId] = null
 
   return bears
 }
