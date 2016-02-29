4394aff87c2a70acebf718a9525689054ff14091
Johnny Pesola <johnny@pesola.eu>
2016-02-29 03:59:38 +0100

    More refactoring on sofa component

--- src/app/components/gameView/sofa.jsx
+++ src/app/components/gameView/sofa.jsx
@@ -8,30 +8,37 @@ const Sofa = ( props ) => {
   }
 
   const getSofaStyles = () => {
+
+    const sofaStyles = {
+      twoSeats: {
+        background: 'url(' + C.SRC_TO_IMAGES.SOFAS['2'] + ')',
+        width: applyScale( '450', 'px' ),
+        height: applyScale( '240', 'px' ),
+        padding: `${ applyScale( '20', 'px ' ) } ${ applyScale( '8', 'px' ) } 0 ${ applyScale( '12', 'px' ) }`
+      },
+      threeSeats: {
+        background: 'url(' + C.SRC_TO_IMAGES.SOFAS['3'] + ')',
+        width: applyScale( '500', 'px' ),
+        height: applyScale( '270', 'px' ),
+        padding: `${ applyScale( '40', 'px ' ) } ${ applyScale( '8', 'px' ) } 0 ${ applyScale( '12', 'px' ) }`
+      },
+      fourSeats: {
+        background: 'url(' + C.SRC_TO_IMAGES.SOFAS['4'] + ')',
+        width: applyScale( '600', 'px' ),
+        height: applyScale( '300', 'px' ),
+        padding: `${ applyScale( '70', 'px ' ) } ${ applyScale( '75', 'px' ) } 0 ${ applyScale( '85', 'px' ) }`
+      }
+    }
+
     switch ( props.numberOfSeats ) {
-    case 2: return {
-      background: 'url(' + C.SRC_TO_IMAGES.SOFAS['2'] + ')',
-      width: applyScale( '450', 'px' ),
-      height: applyScale( '240', 'px' ),
-      padding: `${ applyScale( '20', 'px ' ) } ${ applyScale( '8', 'px' ) } 0 ${ applyScale( '12', 'px' ) }`
-    }
-    case 3: return {
-      background: 'url(' + C.SRC_TO_IMAGES.SOFAS['3'] + ')',
-      width: applyScale( '500', 'px' ),
-      height: applyScale( '270', 'px' ),
-      padding: `${ applyScale( '40', 'px ' ) } ${ applyScale( '8', 'px' ) } 0 ${ applyScale( '12', 'px' ) }`
-    }
-    case 4: return {
-      background: 'url(' + C.SRC_TO_IMAGES.SOFAS['4'] + ')',
-      width: applyScale( '600', 'px' ),
-      height: applyScale( '300', 'px' ),
-      padding: `${ applyScale( '70', 'px ' ) } ${ applyScale( '75', 'px' ) } 0 ${ applyScale( '85', 'px' ) }`
-    }
-    default: return C.SRC_TO_IMAGES.SOFAS['4']
+    case 2: return sofaStyles.twoSeats
+    case 3: return sofaStyles.threeSeats
+    case 4: return sofaStyles.fourSeats
+    default: return sofaStyles.fourSeats
     }
   }
 
-  const styles = {
+  const genericStyles = {
     sofa: {
       backgroundSize: 'contain',
       backgroundPosition: 'top',
@@ -43,15 +50,15 @@ const Sofa = ( props ) => {
     }
   }
 
-  // Merge styles: hard defined styles, prop styles, and sofa specific styles
-  const sofaStyles = Object.assign({}, styles.sofa, props.styles, getSofaStyles() )
+  // Merge styles: genericStyles, prop.styles, and sofaStyles
+  const mergedStyles = Object.assign({}, genericStyles.sofa, props.styles, getSofaStyles() )
 
   return (
     <div
       className={ C.COMPONENT_NAMES.SOFA }
-      style={ sofaStyles }
+      style={ mergedStyles }
     >
-      <div style={ styles.seatContainer }>
+      <div style={ genericStyles.seatContainer }>
 
         { props.children }
 
