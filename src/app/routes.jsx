1f0b60849da4c50e3e4a45bb8828cc4aaecada93
cs222wa <cs222wa@student.lnu.se>
2016-02-29 15:26:22 +0100

    changed for conflic resolve

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
-import Wrapper from './components/shared/wrapper.jsx'
+import C from './constants'
 
+import Wrapper from './components/shared/wrapper.jsx'
 import NoMatch from './components/shared/noMatch.jsx'
 import Main from './components/main.jsx'
 import About from './components/about.jsx'
-import Resultview from './components/resultview/resultview'
+import StartView from './components/startView'
+import GameView from './components/gameView'
+import SavedCombinationsView from './components/savedCombinationsView'
+// import ResultsView from './components/resultsView'
 
 export default (
-  <Route path = '/' component = { Wrapper }>
-    <IndexRoute component = { Main } />
-    <Route path = '/about' component = { About }/>
-    <Route path = '/resultview' component = { Resultview }/>
-    <Route path = '*' component = { NoMatch }/>
+  <Route path='/' component={ Wrapper } >
+    <IndexRoute component={ Main } />
+    <Route path={ C.ROUTES.START } component={ StartView } />
+    <Route path={ C.ROUTES.GAME } component={ GameView } />
+    <Route path={ C.ROUTES.SAVED } component={ SavedCombinationsView } />
+    <Route path='*' component={ NoMatch } />
   </Route>
 )
