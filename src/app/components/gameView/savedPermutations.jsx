d93ef619e62af33a8d50292ce18c140694446a72
Johnny Pesola <johnny@pesola.eu>
2016-03-01 15:21:37 +0100

    Fixed savedPermitations components

--- src/app/components/gameView/savedPermutations.jsx
+++ src/app/components/gameView/savedPermutations.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
 import C from '../../constants'
-
+import SavedPermutationsSofaList from './savedPermutationsSofaList'
 import { Link } from 'react-router'
 
 const styles = {
