acc679c25f253d1cf0abf37412f79f39d3914cf1
OskarKlintrot <oskar.klintrot@gmail.com>
2016-03-02 10:36:33 +0100

    First two bears are randomized

--- src/app/redux/store/initialState.js
+++ src/app/redux/store/initialState.js
@@ -1,15 +1,27 @@
 import C from '../../constants'
 
+const randomBearNumber = ( exception ) => {
+  const maxBear = 8
+  let random
+  do
+    random = Math.floor( ( Math.random() * maxBear ) + 0 )
+  while ( random === exception )
+  return random
+}
+
+const firstBear = randomBearNumber()
+const secondBear = randomBearNumber( firstBear )
+
 const InitialState = () => {
   return {
     settings: {
       numberOfSeats: 2,
       bears: {
         0: {
-          src: C.SRC_TO_IMAGES.BEARS.BLUE
+          src: C.SRC_TO_IMAGES.BEARS[C.COLORS_ENUM[firstBear]]
         },
         1: {
-          src: C.SRC_TO_IMAGES.BEARS.GREEN
+          src: C.SRC_TO_IMAGES.BEARS[C.COLORS_ENUM[secondBear]]
         },
         2: null,
         3: null
@@ -18,8 +30,8 @@ const InitialState = () => {
     },
     game: {
       savedPermutations: [],
-      bearsOnSofa: [null, null, null, null],
-      bearsOnStart: ["0", "1", null, null]
+      bearsOnSofa: null,
+      bearsOnStart: null
     }
   }
 }
