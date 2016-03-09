b5b3f34c6134f02570555fa1d03c89fcf9bd5dd2
OskarKlintrot <oskar.klintrot@gmail.com>
2016-03-09 21:20:05 +0100

    Creating the whole manifest from ps-script

---
+++ tools/createManifest.ps1
@@ -0,0 +1,22 @@
+﻿$pathToManifest = '..\src\www\manifest.appcache'
+
+$currentPath = $Pwd
+cd ..\src\www\
+$pathToRemove = $Pwd
+$pathToRemoveString = $pathToRemove.ToString().Replace('\', '\\') + '\\'
+cd $currentPath
+
+# Clear current file
+Out-File -Encoding utf8 $pathToManifest
+
+# Title
+'CACHE MANIFEST' | Out-File -Append -Encoding utf8 $pathToManifest
+
+# A random string in order to update the appcache
+.\createRandomString.ps1 | Out-File -Append -Encoding utf8 $pathToManifest
+
+# All resources being cached
+'CACHE:' | Out-File -Append -Encoding utf8 $pathToManifest
+Get-ChildItem -Path ..\src\www\public -Recurse -File | % { $_.FullName.ToString() -replace($pathToRemoveString, '') -replace('\\', '/') | Out-File -Append -Encoding utf8 $pathToManifest }
+"scripts/boundle.min.js" | Out-File -Append -Encoding utf8 $pathToManifest
+
--- tools/getAllFiles.ps1
+++
@@ -1,9 +0,0 @@
-﻿$currentPath = $Pwd
-cd ..\src\www\
-$pathToRemove = $Pwd
-$pathToRemoveString = $pathToRemove.ToString().Replace('\', '\\')
-cd $currentPath
-Out-File -Encoding utf8 .\filesInPublicDir.txt
-.\createRandomString.ps1 | Out-File -Append -Encoding utf8 .\filesInPublicDir.txt
-Get-ChildItem -Path ..\src\www\public -Recurse -File | % { $_.FullName.ToString() -replace($pathToRemoveString, '') -replace('\\', '/') | Out-File -Append -Encoding utf8 .\filesInPublicDir.txt }
-"/scripts/boundle.min.js" | Out-File -Append -Encoding utf8 .\filesInPublicDir.txt
