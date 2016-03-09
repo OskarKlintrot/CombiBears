4ee699d1ce82c3e22fb53f352a6d44afaf73fbb6
OskarKlintrot <oskar.klintrot@gmail.com>
2016-03-09 21:08:25 +0100

    Create a random string for appcache manifest

--- tools/getAllFiles.ps1
+++ tools/getAllFiles.ps1
@@ -4,5 +4,6 @@ $pathToRemove = $Pwd
 $pathToRemoveString = $pathToRemove.ToString().Replace('\', '\\')
 cd $currentPath
 Out-File -Encoding utf8 .\filesInPublicDir.txt
+.\createRandomString.ps1 | Out-File -Append -Encoding utf8 .\filesInPublicDir.txt
 Get-ChildItem -Path ..\src\www\public -Recurse -File | % { $_.FullName.ToString() -replace($pathToRemoveString, '') -replace('\\', '/') | Out-File -Append -Encoding utf8 .\filesInPublicDir.txt }
 "/scripts/boundle.min.js" | Out-File -Append -Encoding utf8 .\filesInPublicDir.txt
