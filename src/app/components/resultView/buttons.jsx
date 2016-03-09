0107ab54de167dd076fac261085c1d82d395db31
OskarKlintrot <oskar.klintrot@gmail.com>
2016-03-09 09:14:29 +0100

    Button for showing results hides when pressed

--- src/app/components/resultView/buttons.jsx
+++ src/app/components/resultView/buttons.jsx
@@ -10,45 +10,70 @@ const styles = {
   }
 }
 
-const Buttons = ( props ) => {
-  const { handleClick } = props
-  return (
-    <div className='row'>
-      <div className='small-4 medium-6 large-8 columns'>
-        <Link
-          to={ C.ROUTES.START }
+class Buttons extends React.Component {
+  constructor( props ) {
+    super( props )
+    this.state = {
+      showSolution: false
+    }
+  }
+
+  handleShowResult() {
+    this.setState({
+      showSolution: true
+    })
+  }
+
+  render() {
+    const { handleClick } = this.props
+    return (
+      <div className='row'>
+        <div className='small-4 medium-6 large-8 columns'>
+          <Link
+            to={ C.ROUTES.START }
+          >
+            <img
+              src={ C.SRC_TO_IMAGES.ICONS.NEW_SOFA }
+              alt='Icon for new sofa'
+              draggable='false'
+              style={ styles.button }
+            />
+          </Link>
+        </div>
+        <div
+          className='small-4 medium-3 large-2 columns'
+          hidden={ this.state.showSolution }
         >
           <img
-            src={ C.SRC_TO_IMAGES.ICONS.NEW_SOFA }
+            src={ C.SRC_TO_IMAGES.ICONS.SHOW_SOLUTION }
             alt='Icon for new sofa'
             draggable='false'
             style={ styles.button }
+            onClick={ () => {
+              handleClick()
+              this.handleShowResult()
+            } }
           />
-        </Link>
+        </div>
+        <div
+          className='small-4 medium-3 large-2 columns'
+          hidden={ !this.state.showSolution }
+        ></div>
+        <div className='small-4 medium-3 large-2 columns'>
+          <Link
+            to={ C.ROUTES.GAME }
+          >
+            <img
+              src={ C.SRC_TO_IMAGES.ICONS.ARROW_LEFT }
+              alt='Icon for returning to game view'
+              draggable='false'
+              style={ styles.button }
+            />
+          </Link>
+        </div>
       </div>
-      <div className='small-4 medium-3 large-2 columns'>
-        <img
-          src={ C.SRC_TO_IMAGES.ICONS.SHOW_SOLUTION }
-          alt='Icon for new sofa'
-          draggable='false'
-          style={ styles.button }
-          onClick={ handleClick }
-        />
-      </div>
-      <div className='small-4 medium-3 large-2 columns'>
-        <Link
-          to={ C.ROUTES.GAME }
-        >
-          <img
-            src={ C.SRC_TO_IMAGES.ICONS.ARROW_LEFT }
-            alt='Icon for returning to game view'
-            draggable='false'
-            style={ styles.button }
-          />
-        </Link>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 Buttons.propTypes = {
