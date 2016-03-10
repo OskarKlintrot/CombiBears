0c7ef4cba48fa97dddcfe30624507ab8fc7605aa
cs222wa <cs222wa@student.lnu.se>
2016-03-10 09:26:19 +0100

    removed unnecessary code.

--- src/app/components/resultView/foundPermutations.jsx
+++ src/app/components/resultView/foundPermutations.jsx
@@ -1,21 +1,33 @@
 import React, { PropTypes } from 'react'
 import BasicSofa from '../shared/basicSofa-v2'
-import styles from './shared/styles'
 
-const notFoundPermutationsStyle = Object.assign(
-  {},
-  styles.permutation,
-  {
-    opacity: '0.1'
+const styles = {
+  savedPermutations: {
+    overflow: 'auto',
+    backgroundColor: 'rgba(240, 240, 230, 0.8)',
+    border: '0.5em solid rgb(250, 250, 240)',
+    borderRadius: '0.5em',
+    marginTop: '2em',
+    paddingTop: '1em',
+    marginBottom: '1em'
+  },
+  ulSofas: {
+    margin: 0
+  },
+  permutation: {
+    display: 'inline',
+    float: 'left',
+    width: '30%'
   }
-)
+}
+
+const notFoundPermutationsStyle = Object.assign({}, styles.permutation, { opacity: '0.1' })
 
 const ShowFoundPermutations = ( props ) => {
   const correctCombos = props.settings.correctCombinations.map( ( element ) => JSON.stringify( element ) )
   const savedCombos = props.savedPermutations
       .map( ( element ) => JSON.stringify( element ) )
       .map( ( element ) => correctCombos.indexOf( element ) )
-
   return (
     <div className='row'>
       <div
--- src/app/components/resultView/index.jsx
+++ src/app/components/resultView/index.jsx
@@ -1,39 +1,26 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import ResultList from './resultList'
-import FoundList from './foundList'
 import Buttons from './buttons'
+import FoundPermutations from './foundPermutations'
 
-class ResultView extends React.Component {
-  constructor( props ) {
-    super( props )
-    this.state = {
-      showSolutions: false
-    }
-  }
-  onShowSolutions = () => {
-    this.setState({ showSolutions: true })
-  };
-
-  render() {
-    return (
-      <div>
-        <Buttons handleClick={ this.onShowSolutions } />
-        <ResultList
-          className='small-12 medium-9 medium-offset-1 columns'
-          numberOfFoundPermutations={ this.props.game.savedPermutations.length }
-          numberOfCorrectPermutations={ this.props.settings.correctCombinations.length }
-        />
-        <FoundList
-          className='small-12 medium-10 medium-offset-1 columns'
-          showSolutions={ this.state.showSolutions }
-          savedPermutations={ this.props.game.savedPermutations }
-          settings={ this.props.settings }
-          game={ this.props.game }
-        />
-      </div>
-    )
-  }
+const ResultView = ( props ) => {
+  const { game, settings } = props
+  return (
+    <div>
+      <Buttons />
+      <ResultList
+        className='small-12 medium-9 medium-offset-1 columns'
+        numberOfFoundPermutations={ game.savedPermutations.length }
+        numberOfCorrectPermutations={ settings.correctCombinations.length }
+      />
+      <FoundPermutations
+        className='small-12 medium-10 medium-offset-1 columns'
+        savedPermutations={ game.savedPermutations }
+        settings={ settings }
+      />
+    </div>
+  )
 }
 
 const mapStateToProps = ( state ) => {
