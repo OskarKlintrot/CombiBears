4ef2b4e0671a1789db27f50cb4c10c58166e51b8
Johnny Pesola <johnny@pesola.eu>
2016-03-10 04:37:48 +0100

    Gameview now fully responsive, except saved permutations

--- src/app/components/gameView/buttons.jsx
+++ src/app/components/gameView/buttons.jsx
@@ -1,115 +1,157 @@
 import React, { PropTypes } from 'react'
 import C from '../../constants'
 import { Link } from 'react-router'
+import Radium from 'radium'
 
-const Buttons = ( props ) => {
+@Radium
+class Buttons extends React.Component {
 
-  const disabledOpacity = 0.5
-  const enabledOpacity = 1
+  render() {
 
-  const handleBlurBackground = () => {
-    document.getElementById( 'backgroundImage' ).setAttribute( 'style', '-webkit-filter: blur(10px) grayscale(0.3)' )
-  }
+    const disabledOpacity = 0.5
+    const enabledOpacity = 1
 
-  const styles = {
-
-    iconToStart: {
-      width: '15%',
-      cursor: 'pointer',
-      zIndex: 10,
-      float: 'left',
-      margin: '1% 0 0 4%'
-    },
-    iconToShowResults: {
-      width: '15%',
-      cursor: 'pointer',
-      zIndex: 10,
-      float: 'right',
-      margin: '1% 2% 0 0'
-    },
-    resetIcon: {
-      width: '49%',
-      cursor: props.canRestart ? 'pointer' : 'auto',
-      marginRight: '2%',
-      zIndex: 10,
-      opacity: props.canRestart ? enabledOpacity : disabledOpacity,
-      filter: props.canRestart ? 'none' : 'grayscale(1) brightness(1.3)',
-      WebkitFilter: props.canRestart ? 'none' : 'grayscale(1) brightness(1.4)'
-    },
-    saveIcon: {
-      width: '49%',
-      cursor: props.canSave ? 'pointer' : 'auto',
-      zIndex: 10,
-      opacity: props.canSave ? enabledOpacity : disabledOpacity,
-      filter: props.canSave ? 'none' : 'grayscale(100%) brightness(1.3)',
-      WebkitFilter: props.canSave ? 'none' : 'grayscale(100%) brightness(1.4)'
-    },
-    topIconsArea: {
-      top: '0',
-      left: '0',
-      position: 'fixed',
-      width: '80%',
-      zIndex: 10
-
-    },
-    bottomIconsArea: {
-      bottom: '1%',
-      right: '21%',
-      position: 'fixed',
-      width: '23%',
-      zIndex: 10
+    const handleBlurBackground = () => {
+      document.getElementById( 'backgroundImage' ).setAttribute( 'style', '-webkit-filter: blur(10px) grayscale(0.3)' )
     }
+
+    const styles = {
+
+      iconToStart: {
+        width: '100px',
+        '@media (max-width: 1023px)': {
+          width: '80px'
+        },
+        '@media (max-width: 400px)': {
+          width: '60px'
+        },
+        cursor: 'pointer',
+        zIndex: 10,
+        float: 'left'
+      },
+      iconToShowResults: {
+        width: '100px',
+        '@media (max-width: 1023px)': {
+          width: '80px'
+        },
+        '@media (max-width: 400px)': {
+          width: '60px'
+        },
+        cursor: 'pointer',
+        zIndex: 10,
+        float: 'right'
+      },
+      resetIcon: {
+        width: '100px',
+        '@media (max-width: 1023px)': {
+          width: '80px'
+        },
+        '@media (max-width: 400px)': {
+          width: '60px'
+        },
+        cursor: this.props.canRestart ? 'pointer' : 'auto',
+        marginRight: '2%',
+        zIndex: 10,
+        opacity: this.props.canRestart ? enabledOpacity : disabledOpacity,
+        filter: this.props.canRestart ? 'none' : 'grayscale(1) brightness(1.3)',
+        WebkitFilter: this.props.canRestart ? 'none' : 'grayscale(1) brightness(1.4)'
+      },
+      saveIcon: {
+        width: '100px',
+        '@media (max-width: 1023px)': {
+          width: '80px'
+        },
+        '@media (max-width: 400px)': {
+          width: '60px'
+        },
+        cursor: this.props.canSave ? 'pointer' : 'auto',
+        zIndex: 10,
+        opacity: this.props.canSave ? enabledOpacity : disabledOpacity,
+        filter: this.props.canSave ? 'none' : 'grayscale(100%) brightness(1.3)',
+        WebkitFilter: this.props.canSave ? 'none' : 'grayscale(100%) brightness(1.4)'
+      },
+      topIconsArea: {
+        left: '0',
+        padding: '2%',
+        position: 'fixed',
+        width: '80%',
+        zIndex: 10
+
+      },
+      bottomIconsArea: {
+        bottom: '1%',
+        right: '21%',
+        position: 'fixed',
+        zIndex: 10,
+        textAlign: 'right',
+        width: '80px',
+        '@media (max-height: 500px)': {
+          width: '71px'
+        },
+        '@media (min-width: 900px)': {
+          width: '110px'
+        },
+        '@media (max-width: 512px)': {
+          width: '71px'
+        },
+        '@media (max-width: 479px)': {
+          width: '130px',
+          bottom: 'auto',
+          top: '100px'
+        }
+      }
+    }
+
+    return (
+
+      <div>
+        <div style={ styles.topIconsArea }>
+
+          <Link
+            to={ C.ROUTES.START }
+            onClick={ handleBlurBackground }
+          >
+            <img
+              src={ C.SRC_TO_IMAGES.ICONS.NEW_SOFA }
+              alt='Icon for new sofa'
+              style={ styles.iconToStart }
+              draggable='false'
+            />
+          </Link>
+          <Link
+            to={ C.ROUTES.RESULTS }
+            onClick={ handleBlurBackground }
+          >
+            <img
+              src={ C.SRC_TO_IMAGES.ICONS.SHOW_RESULT }
+              alt='Icon for showing result'
+              style={ styles.iconToShowResults }
+              draggable='false'
+            />
+          </Link>
+
+        </div>
+
+        <div style={ styles.bottomIconsArea }>
+          <img
+            src={ C.SRC_TO_IMAGES.ICONS.RESTART }
+            alt='Icon for putting bears back in startingArea'
+            style={ styles.resetIcon }
+            onClick={ () => this.props.onRestart() }
+            draggable='false'
+          />
+          <img
+            src={ C.SRC_TO_IMAGES.ICONS.SCREENSHOT }
+            alt='Icon for saving permutation'
+            style={ styles.saveIcon }
+            onClick={ () => this.props.onSave() }
+            draggable='false'
+          />
+        </div>
+
+      </div>
+    )
   }
-
-  return (
-
-    <div>
-      <div style={ styles.topIconsArea }>
-
-        <Link
-          to={ C.ROUTES.START }
-          onClick={ handleBlurBackground }
-        >
-          <img
-            src={ C.SRC_TO_IMAGES.ICONS.NEW_SOFA }
-            alt='Icon for new sofa'
-            style={ styles.iconToStart }
-            draggable='false'
-          />
-        </Link>
-        <Link
-          to={ C.ROUTES.RESULTS }
-          onClick={ handleBlurBackground }
-        >
-          <img
-            src={ C.SRC_TO_IMAGES.ICONS.SHOW_RESULT }
-            alt='Icon for showing result'
-            style={ styles.iconToShowResults }
-            draggable='false'
-          />
-        </Link>
-
-      </div>
-
-      <div style={ styles.bottomIconsArea }>
-        <img
-          src={ C.SRC_TO_IMAGES.ICONS.RESTART }
-          alt='Icon for putting bears back in startingArea'
-          style={ styles.resetIcon }
-          onClick={ () => props.onRestart() }
-          draggable='false'
-        />
-        <img
-          src={ C.SRC_TO_IMAGES.ICONS.SCREENSHOT }
-          alt='Icon for saving permutation'
-          style={ styles.saveIcon }
-          onClick={ () => props.onSave() }
-          draggable='false'
-        />
-      </div>
-
-    </div>
-  )
 }
 
 Buttons.propTypes = {
