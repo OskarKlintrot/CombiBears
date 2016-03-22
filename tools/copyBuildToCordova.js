8233b7ea5935185917e0a7a71b695171dc077f04
OskarKlintrot <oskar.klintrot@gmail.com>
2016-03-22 13:40:52 +0100

    Added new scripts

---
+++ tools/copyBuildToCordova.js
@@ -0,0 +1,24 @@
+// This script works but can't be used since node is not running
+// with proper permission on windows when using npm scripts
+
+var fs = require('fs')
+var ncp = require('ncp')
+var rimraf = require('rimraf')
+var path = require('path')
+var buildPath = path.resolve(__dirname, '../build')
+var cordovaPath = path.resolve(__dirname, '../cordova/www')
+var keepPath = path.resolve(__dirname, '../cordova/www/.keep')
+
+try {
+  rimraf.sync(cordovaPath)
+} catch (e) {
+  console.error(e)
+} finally {
+  ncp(buildPath, cordovaPath, function (err) {
+   if (err) {
+     return console.error(err)
+   }
+  //  fs.closeSync(fs.openSync(keepPath, 'w'))
+   console.log('Build copied to Cordova!')
+  })
+}
